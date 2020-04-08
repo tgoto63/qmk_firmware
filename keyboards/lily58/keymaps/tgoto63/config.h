244c19d719aa48659083dfbbbe03c879dfc26360
tgoto63 <tgoto63@gmail.com>
2020-04-08 15:05:27 +0900

    Change: tappin term.

--- keyboards/lily58/keymaps/tgoto63/config.h
+++ keyboards/lily58/keymaps/tgoto63/config.h
@@ -34,7 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #undef TAPPING_TERM
-#define TAPPING_TERM 100
+#define TAPPING_TERM 150
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
